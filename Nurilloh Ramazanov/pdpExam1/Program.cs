0a67101e818d2daaae72cfb2266e8ce72294b5b5
Nurilloh <nurilloh6@gmail.com>
2022-11-11 12:40:18 +0500

    Nurilloh Ramazanov

---
+++ Nurilloh Ramazanov/pdpExam1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+namespace algoritm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MinimalLength(new int[] { 1,1,1,1,1,1,1,1,1,1 }, 11));
+        Console.WriteLine(SumTwoNumbersAsString("11", "123"));
+        Console.ReadLine();
+    }
+
+    static string Hexadecimal(int num)
+    {
+
+
+
+        return "fffffff";
+    }
+
+
+    static string SumTwoNumbersAsString(string num1, string num2)
+    {
+        if (num1 == "0" && num2 == "0")
+            return "0";
+        string res = "";
+        int num1Length = num1.Length-1;
+        int num2Length = num2.Length-1;
+        string result = "";
+        string point = "";
+        //11 123
+        while(num1Length > -1 || num2Length > -1)
+        {
+            if(num1Length == -1)
+            {
+                res = (('0' - '0') + (num2[num2Length--] - '0').ToString());
+            }
+            else if(num2Length == -1)
+            {
+                res = ((char)(num1[num1Length--] - '0') + (char)('0' - '0')).ToString();
+            }
+            else
+            {
+                res = ( (char)(num1[num1Length--] - '0') + (char)(num2[num2Length--] - '0')).ToString();
+            }
+
+            
+            
+            if(res.Length > 1)
+            {
+                if (point.Length > 0)
+                {
+                    res = res[0] + ((char)res[1] - '0' + point[0] - '0').ToString();
+                    point = "";
+                }
+                point = ( (char)res[0] - '0').ToString();
+                res = res.Substring(1);
+            }
+            else
+            {
+                res = ((char)res[0] - '0' + (char)point[0] - '0').ToString();
+            }
+            
+            result = res + result;
+            
+
+        }
+        return result;
+    }
+
+    static int MinimalLength(int[] arr, int target)
+    {
+        int count = 0;
+        int result = 0;
+        int res = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            res += arr[i];
+            count++;
+            if (res == target)
+            {
+                result = count;
+                count = 0;
+                res = 0;
+            }
+            if(res > target)
+            {
+                res = 0;
+                count = 0;
+            }     
+        }
+
+        return result;
+    }
+}
---
+++ Nurilloh Ramazanov/pdpexam/Classes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace pdpexam
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public double[] GeoCode { get; set; } = new double[2];
+        public long TgId { get; set; }
+        public Status UserStatus { get; set; }
+    }
+
+    public enum Status
+    {
+        name,
+        number,
+        geocode,
+        registred
+    }
+
+    public class FilterMapping
+    {
+    }
+
+    public class Result
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public string link { get; set; }
+        public string type { get; set; }
+        public int relevance { get; set; }
+        public string content { get; set; }
+        public List<object> dataPoints { get; set; }
+    }
+
+    public class Root
+    {
+        public string sorting { get; set; }
+        public FilterMapping filterMapping { get; set; }
+        public List<object> filterOptions { get; set; }
+        public List<object> activeFilterOptions { get; set; }
+        public string query { get; set; }
+        public int totalResults { get; set; }
+        public int limit { get; set; }
+        public int offset { get; set; }
+        public List<SearchResult> searchResults { get; set; }
+        public long expires { get; set; }
+        public bool isStale { get; set; }
+    }
+
+    public class SearchResult
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public int totalResults { get; set; }
+        public int totalResultsVariants { get; set; }
+        public List<Result> results { get; set; }
+    }
+
+
+}
+
